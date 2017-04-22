ca44e6c3f8b981b8f8c851a25135a9a1dad5f3d5
amillikin <noname102190@gmail.com>
2017-04-22 09:38:37 -0400

    Everything working except shiftAmt during padding

--- SHA512/SHA512.cpp
+++ SHA512/SHA512.cpp
@@ -25,18 +25,26 @@ typedef unsigned long long ull;
 ifstream inFile;
 
 struct hashStruct {
-	ull messageBlock[16];
-	ull w[80];
-	ull a;
-	ull b;
-	ull c;
-	ull d;
-	ull e;
-	ull f;
-	ull g;
-	ull h;
-	ull mixer1;
-	ull mixer2;
+	ull messageBlock[16] = { 0 };
+	ull w[80] = { 0 };
+	ull a = 0;
+	ull b = 0;
+	ull c = 0;
+	ull d = 0;
+	ull e = 0;
+	ull f = 0;
+	ull g = 0;
+	ull h = 0;
+	ull initA = 0;
+	ull initB = 0;
+	ull initC = 0;
+	ull initD = 0;
+	ull initE = 0;
+	ull initF = 0;
+	ull initG = 0;
+	ull initH = 0;
+	ull mixer1 = 0;
+	ull mixer2 = 0;
 };
 
 // Initial Hash Values
@@ -65,7 +73,7 @@ static const ull roundConst[80]{
 	0x431d67c49c100d4c, 0x4cc5d4becb3e42b6, 0x597f299cfc657e2a, 0x5fcb6fab3ad6faec, 0x6c44198c4a475817
 };
 
-// Creates necessary hex bytes to and with buffer that should contain less than 8 bytes
+// Gets necessary hex bytes to create a mask
 ull getMask(size_t maskAmt) {
 	ull mask = 0;
 	for (size_t i = 0; i < maskAmt; i++) {
@@ -77,22 +85,22 @@ ull getMask(size_t maskAmt) {
 
 // Right Rotate
 ull rr(ull rotWord, int rotAmt) {
-	return ((rotWord & getMask(rotAmt)) << rotAmt) || (rotWord >> rotAmt);
+	return _rotr64(rotWord, rotAmt);
 }
 
 // Right Shift
 ull rs(ull shiftWord, int shiftAmt) {
-	return shiftWord >> shiftAmt;
+	return __ull_rshift(shiftWord, shiftAmt);
 }
 
 // Majority
 ull maj(ull a, ull b, ull c) {
-	return (a & b) ^ (a & c) ^ (b & c);
+	return ((a & b) ^ (a & c) ^ (b & c));
 }
 
 // Conditional
 ull cond(ull e, ull f, ull g) {
-	return (e & f) ^ (~e & g);
+	return ((e & f) ^ (~e & g));
 }
 
 // Rotates A for use in determining mixer1
@@ -109,7 +117,7 @@ ull rotE(ull e) {
 // then fills the remainder of the array with the calculated value
 hashStruct fillWordArray(hashStruct block) {
 	for (int i = 0; i < 16; i++) {
-		block.w[i] = _byteswap_uint64(block.messageBlock[i]);
+		block.w[i] = block.messageBlock[i];
 	}
 	for (int i = 16; i < 80; i++) {
 		block.w[i] = block.w[i - 16] + (rr(block.w[i - 15], 1) ^ rr(block.w[i - 15], 8) ^ rs(block.w[i - 15], 7))
@@ -135,12 +143,33 @@ hashStruct hashCompression(hashStruct block) {
 		block.b = block.a;
 		block.a = block.mixer1 + block.mixer2;
 	}
+	// Final adding of last round's digest with initial digest
+	block.a = block.a + block.initA;
+	block.b = block.b + block.initB;
+	block.c = block.c + block.initC;
+	block.d = block.d + block.initD;
+	block.e = block.e + block.initE;
+	block.f = block.f + block.initF;
+	block.g = block.g + block.initG;
+	block.h = block.h + block.initH;
+
+	// Saving initial digest values for next round
+	block.initA = block.a;
+	block.initB = block.b;
+	block.initC = block.c;
+	block.initD = block.d;
+	block.initE = block.e;
+	block.initF = block.f;
+	block.initG = block.g;
+	block.initH = block.h;
+
 	return block;
 }
 
 //Writes a state to the outfile
 void writeHash(hashStruct block) {
 	cout << hex;
+	cout << fixed << setprecision(16);
 	cout << block.a << " ";
 	cout << block.b << " ";
 	cout << block.c << " ";
@@ -163,12 +192,17 @@ hashStruct padBlock(hashStruct block, ull fileSize, int bytesLeft) {
 		// Then create one final block of 0x0 and the final word being the file length
 		// Else we can just append 0x80 where the file ends, 
 		// set the last word to file length, and fill between with 0x0
-		inFile.read(reinterpret_cast<char*>(&block.messageBlock), sizeof(block.messageBlock)-bytesLeft);
-		int padStart = ((128 - bytesLeft) / 16);
-		int shiftAmt = ((128 - bytesLeft) % 16);
+		inFile.read(reinterpret_cast<char*>(&block.messageBlock), sizeof(block.messageBlock));
+		int padStart = 7 - ((128 - bytesLeft) / 16);
+		int shiftAmt = (((128 - bytesLeft) % 16)-9)*8;
+		ull appendOne = 0x80;
 
-		if (bytesLeft < 9) {
-			block.messageBlock[padStart] ^= (0x80 << (shiftAmt - 1));
+		for (int i = 0; i <= padStart; i++) {
+			block.messageBlock[i] = _byteswap_uint64(block.messageBlock[i]);
+		}
+
+		if ((128 - bytesLeft) < 9) {
+			block.messageBlock[padStart] |= (appendOne << shiftAmt);
 			for (int i = (padStart + 1); i < 16; i++) {
 				block.messageBlock[i] = 0x0;
 			}
@@ -177,15 +211,15 @@ hashStruct padBlock(hashStruct block, ull fileSize, int bytesLeft) {
 			for (int i = 0; i < 15; i++) {
 				block.messageBlock[i] = 0x0;
 			}
-			block.messageBlock[15] = fileSize;
+			block.messageBlock[15] = fileSize*8;
 			block = hashCompression(block);
 		}
 		else {
-			block.messageBlock[padStart] ^= (0x80 << (shiftAmt - 1));
+			block.messageBlock[padStart] |= (appendOne << shiftAmt);
 			for (int i = (padStart + 1); i < 15; i++) {
 				block.messageBlock[i] = 0x0;
 			}
-			block.messageBlock[15] = fileSize;
+			block.messageBlock[15] = fileSize*8;
 			block = hashCompression(block);
 		}
 	}
@@ -194,7 +228,7 @@ hashStruct padBlock(hashStruct block, ull fileSize, int bytesLeft) {
 		for (int i = 1; i < 15; i++) {
 			block.messageBlock[i] = 0x0;
 		}
-		block.messageBlock[15] = fileSize;
+		block.messageBlock[15] = fileSize*8;
 		block = hashCompression(block);
 	}
 	return block;
@@ -247,12 +281,36 @@ int main(int argc, char* argv[]) {
 	block.f = initHashVals[5];
 	block.g = initHashVals[6];
 	block.h = initHashVals[7];
+	block.initA = initHashVals[0];
+	block.initB = initHashVals[1];
+	block.initC = initHashVals[2];
+	block.initD = initHashVals[3];
+	block.initE = initHashVals[4];
+	block.initF = initHashVals[5];
+	block.initG = initHashVals[6];
+	block.initH = initHashVals[7];
 
 	// Read file for duration of count determined earlier (amount of full 128-bit blocks available)
 	// pass through AES, write to outFile.
 	while (readCnt > 0) {
 		readCnt--;
 		inFile.read(reinterpret_cast<char*>(&block.messageBlock), sizeof(block.messageBlock));
+		block.messageBlock[0] = _byteswap_uint64(block.messageBlock[0]);
+		block.messageBlock[1] = _byteswap_uint64(block.messageBlock[1]);
+		block.messageBlock[2] = _byteswap_uint64(block.messageBlock[2]);
+		block.messageBlock[3] = _byteswap_uint64(block.messageBlock[3]);
+		block.messageBlock[4] = _byteswap_uint64(block.messageBlock[4]);
+		block.messageBlock[5] = _byteswap_uint64(block.messageBlock[5]);
+		block.messageBlock[6] = _byteswap_uint64(block.messageBlock[6]);
+		block.messageBlock[7] = _byteswap_uint64(block.messageBlock[7]);
+		block.messageBlock[8] = _byteswap_uint64(block.messageBlock[8]);
+		block.messageBlock[9] = _byteswap_uint64(block.messageBlock[9]);
+		block.messageBlock[10] = _byteswap_uint64(block.messageBlock[10]);
+		block.messageBlock[11] = _byteswap_uint64(block.messageBlock[11]);
+		block.messageBlock[12] = _byteswap_uint64(block.messageBlock[12]);
+		block.messageBlock[13] = _byteswap_uint64(block.messageBlock[13]);
+		block.messageBlock[14] = _byteswap_uint64(block.messageBlock[14]);
+		block.messageBlock[15] = _byteswap_uint64(block.messageBlock[15]);
 		block = hashCompression(block);
 	}
 	
